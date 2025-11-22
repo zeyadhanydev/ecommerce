39097a519c004fc12d82ead023758aba4d23a526
Loai Wael <princeloai70@gmail.com>
2025-11-23 01:05:08 +0200

    UPD product card layout

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Product } from '../types';
-import { motion } from 'framer-motion';
-import { Star } from 'lucide-react'
+import { Star } from 'lucide-react';
+import { useCart } from '../contexts/CartContext';
+import addToBag from '../assets/svg/add-to-shopping-bag.svg';
 
 interface ProductCardProps {
   product: Product;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const { addToCart } = useCart();
   const ratingCheck = (bg = false): string => {
     const rating = product.rating.rate;
 
@@ -24,27 +26,25 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   }
 
   return (
-    <motion.div
-      whileHover={{ y: -5 }}
-      className="group border-[1px] border-brand-gray rounded-xl p-3 shadow-brand-black/5 shadow-md"
-    >
-      <Link to={`/products/${product.id}`} className="flex flex-col items-start">
+    <div className="group border-2 border-brand-gray/60 rounded-2xl shadow-brand-black/5 shadow-md relative">
+      <Link to={`/products/${product.id}`} className="flex flex-col items-start p-3">
         <div className="bg-brand-gray-light w-full aspect-square overflow-hidden mb-4 rounded-lg">
           <img
             src={product.image || ''}
             alt={product.title}
-            className="w-full h-full object-cover object-center group-hover:scale-105 transition-transform duration-300"
+            className="w-full h-full object-cover object-center transition-transform duration-300"
           />
         </div>
-        <h3 className="text-base font-medium uppercase text-brand-black mb-1 h-12 overflow-hidden">{product.title}</h3>
-        <div className={`py-[0.125rem] px-2 rounded-lg flex items-center gap-1 ${ratingCheck(true)}`}>
+        <h3 className="text-base font-medium uppercase text-brand-black mb-1 h-12 overflow-hidden" title={product.title}>{product.title}</h3>
+        <div className={`py-[0.125rem] px-2 mt-1 rounded-lg flex items-center gap-1 ${ratingCheck(true)}`}>
           <small className='font-bold '>{product.rating.rate}</small>
           <Star className={`h-3 w-3 fill-current ${ratingCheck()}`} />
           <small className='ml-[0.125rem]'>({product.rating.count})</small>
         </div>
         <p className="text-base font-semibold text-brand-black mt-1">€{product.price.toFixed(2)}</p>
       </Link>
-    </motion.div>
+      <button className=' absolute bottom-3 right-3 bg-brand-gray/50 p-1 rounded-xl border-[1px] border-brand-gray-dark/60 hover:opacity-60 transition-opacity' title='Add to Bag' onClick={() => addToCart(product, 1)}><img className='w-7 aspect-square' src={addToBag} alt="Add to Bag" /></button>
+    </div>
   );
 };
 
